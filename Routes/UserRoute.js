f6dba7fce059e1349cae9d0af5ec430c76f5c8a8
Ab-Bakr <abubakarbabagana@webfikasolutions.com>
2022-10-10 10:42:51 -0400

    pass auth

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.get("/:id", getUser);
 router.put("/:id", authMiddleWare, updateUser);
-router.delete("/:id", authMiddleWare, deleteUser);
+router.delete("/:id", deleteUser);
 router.get("/id/:username", getUserId);
 router.put("/:id/follow", followUser);
 router.put("/:id/unfollow", unFollowUser);
